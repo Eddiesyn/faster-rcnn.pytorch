6b8cb72b7678ff13c702009b64d1d39e47c241f6
Eddiesyn <eddiesyn20123950@gmail.com>
2020-03-31 03:55:31 +0200

    run through kinetics

--- video_demo.py
+++ video_demo.py
@@ -226,144 +226,148 @@ if __name__ == '__main__':
     dst_root = './outs/kinetics600'
 
     '''video writer'''
-    cap = cv2.VideoCapture(args.video_path)
-    vids_name = os.path.basename(args.video_path)
-    vids_name = os.path.splitext(vids_name)[0]
-    out_txt = os.path.join(args.outsdir, vids_name+'.txt')
+    # cap = cv2.VideoCapture(args.video_path)
+    # vids_name = os.path.basename(args.video_path)
+    # vids_name = os.path.splitext(vids_name)[0]
+    # out_txt = os.path.join(args.outsdir, vids_name+'.txt')
     # frame_height = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
     # frame_width = cap.get(cv2.CAP_PROP_FRAME_WIDTH)
     # output_vids = os.path.join(args.outdir, 'detected.avi')
     # writer = cv2.VideoWriter(output_vids, cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'), 5,
     #                          (int(frame_width), int(frame_height)))
-    # actions = os.listdir(src_root)
+
+    actions = os.listdir(src_root)
 
     '''meter'''
     detect_meter = AverageMeter()
     nms_meter = AverageMeter()
 
     print('Start detecting...')
-    # for action_idx, action in enumerate(actions):
-    #     print('\nAction {}: {}'.format(action_idx, action))
+    for action_idx, action in enumerate(actions):
+        print('\nAction {}: {}'.format(action_idx + 1, action))
+        video_files = os.listdir(os.path.join(src_root, action))
+        print('{} videos in total'.format(len(video_files)))
 
-    infile = open(out_txt)
-    lines = ""
-    num_frame = 0
-    while(cap.isOpened()):
-        ret, frame = cap.read()
-        if ret is False:
-            break
-        if frame is None:
-            continue
-        num_frame += 1
-        im_in = np.array(frame)
-        if len(im_in.shape) == 2:
-            im_in = im_in[:, :, np.newaxis]
-            im_in = np.concatenate((im_in, im_in, im_in), axis=2)
-        # rgb -> bgr
-        # im = im_in[:, :, ::-1]
-        im = im_in
+        for video_idx, video in enumerate(video_files):
+            video_name = os.path.splitext(video)[0]
+            print('\nVideo {}/{}'.format(video_idx + 1, len(video_files)))
 
-        blobs, im_scales = _get_image_blob(im)
-        assert len(im_scales) == 1, "Only single-image batch implementated"
-        im_blob = blobs
-        im_info_np = np.array(
-            [[im_blob.shape[1], im_blob.shape[2], im_scales[0]]], dtype=np.float32)
+            '''writer'''
+            cap = cv2.VideoCapture(os.path.join(src_root, action, video))
+            out_txt = os.path.join(args.outdir, video_name + '.txt')
+            infile = open(out_txt, 'w')
+            lines = ""
+            num_frame = 0
 
-        im_data_pt = torch.from_numpy(im_blob)
-        im_data_pt = im_data_pt.permute(0, 3, 1, 2)  # (NCHW)
-        im_info_pt = torch.from_numpy(im_info_np)
+            while(cap.isOpened()):
+                ret, frame = cap.read()
+                if ret is False:
+                    break
+                if frame is None:
+                    continue
+                num_frame += 1
+                im_in = np.array(frame)
+                if len(im_in.shape) == 2:
+                    im_in = im_in[:, :, np.newaxis]
+                    im_in = np.concatenate((im_in, im_in, im_in), axis=2)
+                # rgb -> bgr
+                # im = im_in[:, :, ::-1]
+                im = im_in
 
-        with torch.no_grad():
-            im_data.resize_(im_data_pt.size()).copy_(im_data_pt)
-            im_info.resize_(im_info_pt.size()).copy_(im_info_pt)
-            gt_boxes.resize_(1, 1, 5).zero_()
-            num_boxes.resize_(1).zero_()
+                blobs, im_scales = _get_image_blob(im)
+                assert len(im_scales) == 1, "Only single-image batch implementated"
+                im_blob = blobs
+                im_info_np = np.array(
+                    [[im_blob.shape[1], im_blob.shape[2], im_scales[0]]], dtype=np.float32)
 
-        det_tic = time.time()
+                im_data_pt = torch.from_numpy(im_blob)
+                im_data_pt = im_data_pt.permute(0, 3, 1, 2)  # (NCHW)
+                im_info_pt = torch.from_numpy(im_info_np)
 
-        rois, cls_prob, bbox_pred, \
-            rpn_loss_cls, rpn_loss_box, \
-            RCNN_loss_cls, RCNN_loss_bbox, \
-            rois_label = fasterRCNN(im_data, im_info, gt_boxes, num_boxes)
+                with torch.no_grad():
+                    im_data.resize_(im_data_pt.size()).copy_(im_data_pt)
+                    im_info.resize_(im_info_pt.size()).copy_(im_info_pt)
+                    gt_boxes.resize_(1, 1, 5).zero_()
+                    num_boxes.resize_(1).zero_()
 
-        scores = cls_prob.detach()
-        boxes = rois.data[:, :, 1:5]
+                det_tic = time.time()
 
-        if cfg.TEST.BBOX_REG:
-            # Apply bbox regression delta
-            box_deltas = bbox_pred.detach()
-            if cfg.TRAIN.BBOX_NORMALIZE_TARGETS_PRECOMPUTED:
-                # Optionally normalize targets by a precomputed mean and stdv
-                if args.class_agnostic:
-                    if args.cuda > 0:
-                        box_deltas = box_deltas.view(-1, 4) * torch.FloatTensor(cfg.TRAIN.BBOX_NORMALIZE_STDS).cuda() \
-                            + torch.FloatTensor(cfg.TRAIN.BBOX_NORMALIZE_MEANS).cuda()
-                    else:
-                        box_deltas = box_deltas.view(-1, 4) * torch.FloatTensor(cfg.TRAIN.BBOX_NORMALIZE_STDS) \
-                            + torch.FloatTensor(cfg.TRAIN.BBOX_NORMALIZE_MEANS)
-                    box_deltas = box_deltas.view(1, -1, 4)
+                rois, cls_prob, bbox_pred, \
+                    rpn_loss_cls, rpn_loss_box, \
+                    RCNN_loss_cls, RCNN_loss_bbox, \
+                    rois_label = fasterRCNN(im_data, im_info, gt_boxes, num_boxes)
+
+                scores = cls_prob.detach()
+                boxes = rois.data[:, :, 1:5]
+
+                if cfg.TEST.BBOX_REG:
+                    # Apply bbox regression delta
+                    box_deltas = bbox_pred.detach()
+                    if cfg.TRAIN.BBOX_NORMALIZE_TARGETS_PRECOMPUTED:
+                        # Optionally normalize targets by a precomputed mean and stdv
+                        if args.class_agnostic:
+                            if args.cuda > 0:
+                                box_deltas = box_deltas.view(-1, 4) * torch.FloatTensor(cfg.TRAIN.BBOX_NORMALIZE_STDS).cuda() \
+                                    + torch.FloatTensor(cfg.TRAIN.BBOX_NORMALIZE_MEANS).cuda()
+                            else:
+                                box_deltas = box_deltas.view(-1, 4) * torch.FloatTensor(cfg.TRAIN.BBOX_NORMALIZE_STDS) \
+                                    + torch.FloatTensor(cfg.TRAIN.BBOX_NORMALIZE_MEANS)
+                            box_deltas = box_deltas.view(1, -1, 4)
+                        else:
+                            if args.cuda > 0:
+                                box_deltas = box_deltas.view(-1, 4) * torch.FloatTensor(cfg.TRAIN.BBOX_NORMALIZE_STDS).cuda() \
+                                    + torch.FloatTensor(cfg.TRAIN.BBOX_NORMALIZE_MEANS).cuda()
+                            else:
+                                box_deltas = box_deltas.view(-1, 4) * torch.FloatTensor(cfg.TRAIN.BBOX_NORMALIZE_STDS) \
+                                    + torch.FloatTensor(cfg.TRAIN.BBOX_NORMALIZE_MEANS)
+                            box_deltas = box_deltas.view(1, -1, 4 * len(classes))
+                    pred_boxes = bbox_transform_inv(boxes, box_deltas, 1)
+                    pred_boxes = clip_boxes(pred_boxes, im_info.data, 1)
                 else:
-                    if args.cuda > 0:
-                        box_deltas = box_deltas.view(-1, 4) * torch.FloatTensor(cfg.TRAIN.BBOX_NORMALIZE_STDS).cuda() \
-                            + torch.FloatTensor(cfg.TRAIN.BBOX_NORMALIZE_MEANS).cuda()
+                    # Simply repeat the boxes, once for each class
+                    pred_boxes = np.tile(boxes, (1, scores.shape[1]))
+
+                pred_boxes /= im_scales[0]
+
+                scores = scores.squeeze()
+                pred_boxes = pred_boxes.squeeze()
+                detect_meter.update(time.time() - det_tic)
+
+                misc_tic = time.time()
+
+                inds = torch.nonzero(scores[:, person_id] > thresh).view(-1)
+                # if there's a det
+                bbox = [str(num_frame)]
+                if inds.numel() > 0:
+                    cls_scores = scores[:, person_id][inds]
+                    _, order = torch.sort(cls_scores, 0, True)
+                    if args.class_agnostic:
+                        cls_boxes = pred_boxes[inds, :]
                     else:
-                        box_deltas = box_deltas.view(-1, 4) * torch.FloatTensor(cfg.TRAIN.BBOX_NORMALIZE_STDS) \
-                            + torch.FloatTensor(cfg.TRAIN.BBOX_NORMALIZE_MEANS)
-                    box_deltas = box_deltas.view(1, -1, 4 * len(classes))
-            pred_boxes = bbox_transform_inv(boxes, box_deltas, 1)
-            pred_boxes = clip_boxes(pred_boxes, im_info.data, 1)
-        else:
-            # Simply repeat the boxes, once for each class
-            pred_boxes = np.tile(boxes, (1, scores.shape[1]))
+                        cls_boxes = pred_boxes[inds][:, person_id * 4: (person_id + 1) * 4]
+                    cls_dets = torch.cat((cls_boxes, cls_scores.unsqueeze(1)), 1)
+                    cls_dets = cls_dets[order]
+                    keep = nms(cls_boxes[order, :], cls_scores[order], cfg.TEST.NMS)
+                    cls_dets = cls_dets[keep.view(-1).long()]
+                    person_nums = cls_dets.shape[0]
 
-        pred_boxes /= im_scales[0]
+                    dets = cls_dets.cpu().numpy()
+                    for i in range(0, person_nums):
+                        bbox += list(str(int(np.round(x))) for x in dets[i, :4])
+                else:
+                    person_nums = 0
 
-        scores = scores.squeeze()
-        pred_boxes = pred_boxes.squeeze()
-        detect_meter.update(time.time() - det_tic)
+                lines += ",".join(bx for bx in bbox)
+                lines += '\n'
 
-        # im2show = np.copy(im)
-        misc_tic = time.time()
+                nms_meter.update(time.time() - misc_tic)
 
-        inds = torch.nonzero(scores[:, person_id] > thresh).view(-1)
-        # pdb.set_trace()
-        # if there's a det
-        bbox = [str(num_frame)]
-        if inds.numel() > 0:
-            cls_scores = scores[:, person_id][inds]
-            _, order = torch.sort(cls_scores, 0, True)
-            if args.class_agnostic:
-                cls_boxes = pred_boxes[inds, :]
-            else:
-                cls_boxes = pred_boxes[inds][:, person_id * 4: (person_id + 1) * 4]
-            cls_dets = torch.cat((cls_boxes, cls_scores.unsqueeze(1)), 1)
-            cls_dets = cls_dets[order]
-            keep = nms(cls_boxes[order, :], cls_scores[order], cfg.TEST.NMS)
-            cls_dets = cls_dets[keep.view(-1).long()]
-            person_nums = cls_dets.shape[0]
-            # im2show = vis_detections(
-            #     im2show, classes[person_id], cls_dets.cpu().numpy(), 0.5)
-            for i in range(0, len(person_nums)):
-                # bbox = tuple(int(np.round(x)) for x in cls_dets[i, :4])
-                bbox += list(str(int(np.round(x))) for x in cls_dets[i, :4])
-        else:
-            person_nums = 0
+                if num_frame % 100 == 0:
+                    print('detect time {det_meter.val:.3f}({det_meter.avg:.3f})\t'
+                            'nms time {nms_meter.val:.3f}({nms_meter.avg:.3f})'.format(
+                                det_meter=detect_meter, nms_meter=nms_meter
+                            ))
 
-        lines += ",".join(bx for bx in bbox)
-        lines += '\n'
-
-        nms_meter.update(time.time() - misc_tic)
-
-        print('Frame {0}: {1} persons detected, detect time {det_meter.val:.3f}({det_meter.avg:.3f})\t'
-                'nms time {nms_meter.val:.3f}({nms_meter.avg:.3f})'.format(
-                    num_frame, person_nums, det_meter=detect_meter, nms_meter=nms_meter
-                ))
-
-        # print('writing frame_{}'.format(num_frame))
-        # writer.write(im2show)
-
-    infile.writelines(lines)
-    infile.close()
-    print('Detection complete!')
-    cap.release()
-    # writer.release()
+            infile.writelines(lines)
+            infile.close()
+            cap.release()
